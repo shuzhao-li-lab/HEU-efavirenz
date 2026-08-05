99254ebb599a2ba11ae8dc2715b6e1df64d87424
Shuzhao Li <shuzhao.li@gmail.com>
2026-08-05 14:24:31 -0400

    added data processing folder

--- README.md
+++ README.md
@@ -1,1 +1,35 @@
-# HEU-efavirenz
+# HEU-efavirenz metabolomics
+
+This is the repository for data analysis in:
+Distinct biochemical phenotypes of HIV exposed infants driven by antiviral medication
+Jasmine Chong1,#, Shujian Zheng1,#, Jennifer Canniff2, Michael J Johnson2, Elizabeth Aiken1, Joshua M. Mitchell1, Maheshwor Thapa1, Sonali Mishra1, Shabir Madhi6,7, Adriana Weinberg2,3,4,, Shuzhao Li1,8, 
+
+All steps are described under `notebooks/`, using shell or R scripts, notebooks in R or Python:
+
+```
+├── 00_heu_metabolomics_processing
+│   ├── autodrop.json
+│   ├── HILICneg_pcpfm_metadata.csv
+│   ├── HILICpos_pcpfm_metadata.csv
+│   ├── pcpfm_HILICneg.sh
+│   ├── pcpfm_HILICpos.sh
+│   ├── pcpfm_RPneg.sh
+│   ├── pcpfm_RPpos.sh
+│   ├── RPneg_pcpfm_metadata.csv
+│   └── RPpos_pcpfm_metadata.csv
+├── 01_heu_ms1_ms2_annotations
+│   ├── 01_2026-05-21_HEU_ms1_srm_authlib_annotation.ipynb
+│   ├── 02_2026-05-28_HEU_CSM_L4_annotations.ipynb
+│   ├── 03_2026-05-22_HEU_MS2_annotations_MSDial_MONA.ipynb
+│   ├── 04_2026-05-21_building_ms1_library.R
+│   ├── 05_2026-05-22_building_ms2_library.R
+│   └── 06_2026-05-26_combining_heu_annotations.R
+└── 02_heu_metabolomics_analysis
+    └── 01_2025-12-10_HEU_Metabolomics_Analysis.qmd
+```
+
+## Data and Code Availability: 	
+To add.
+
+
+
---
+++ notebooks/00_heu_metabolomics_processing/pcpfm_HILICneg.sh
@@ -0,0 +1,19 @@
+# HILICneg
+
+pcpfm assemble -s 'HILICneg_pcpfm_metadata.csv' -o . -j HEU_08_06_2024_HILICneg
+pcpfm asari -i HEU_08_06_2024_HILICneg
+
+pcpfm blank_masking --table_moniker preferred --new_moniker preferred_blank_masked1 --blank_value Solvent_Blank --sample_value Unknown --query_field "Sample Type" --blank_intensity_ratio 3 -i HEU_08_06_2024_HILICneg 
+pcpfm blank_masking --table_moniker preferred_blank_masked1 --new_moniker preferred_blank_masked2 --blank_value Blank_IS --sample_value Unknown --query_field "Sample Type" --blank_intensity_ratio 3 -i HEU_08_06_2024_HILICneg     
+pcpfm blank_masking --table_moniker preferred_blank_masked2 --new_moniker preferred_blank_masked3 --blank_value Process_Blank --sample_value Unknown --query_field "Sample Type" --blank_intensity_ratio 3 -i HEU_08_06_2024_HILICneg     
+pcpfm drop_samples  --table_moniker preferred_blank_masked3 --new_moniker dropping --drop_value Unknown --drop_field "Sample Type" --drop_others true -i HEU_08_06_2024_HILICneg 
+
+pcpfm drop_outliers --table_moniker dropping --new_moniker dropping --auto_drop autodrop.json -i HEU_08_06_2024_HILICneg
+pcpfm drop_outliers --table_moniker dropping --new_moniker dropping --auto_drop autodrop.json -i HEU_08_06_2024_HILICneg
+
+pcpfm normalize --table_moniker dropping --new_moniker pref_normalized --TIC_normalization_percentile 0.90 -i HEU_08_06_2024_HILICneg 
+pcpfm drop_missing_features --table_moniker pref_normalized --new_moniker pref_missing_dropped --feature_retention_percentile .10  -i HEU_08_06_2024_HILICneg 
+pcpfm impute --table_moniker pref_missing_dropped --new_moniker pref_interpolated -i HEU_08_06_2024_HILICneg     
+pcpfm log_transform --table_moniker pref_interpolated --new_moniker log_transformed_for_analysis -i HEU_08_06_2024_HILICneg
+pcpfm batch_correct --by_batch Batch --table_moniker log_transformed_for_analysis --new_moniker batch_corr -i HEU_08_06_2024_HILICneg
+pcpfm report -i HEU_08_06_2024_HILICneg --color_by='["Batch", "Sample Type"]' --marker_by='["Sample Type"]'
